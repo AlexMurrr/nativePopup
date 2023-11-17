18313a95a3206dffa5e39e1e08a7faead068a702
Alex Mur <lehamuraschov2014@yandex.ru>
2023-11-17 13:12:50 +0200

    feat: print JSON

--- script.js
+++ script.js
@@ -19,20 +19,34 @@ function openPopup() {
   
 function closePopup() {
     popup.style.display = 'none';
-  }
-
+  }  
 function getValue(){
   const surname = document.getElementById('surname').value;
   const name = document.getElementById('name').value; 
   const patronymic = document.getElementById('patronymic').value; 
   const specialization = document.getElementById('specialization').value; 
   const experience = document.getElementById('experience').value;
-  document.getElementById('surnameP').innerHTML=surname;
-  document.getElementById('nameP').innerHTML=name;
-  document.getElementById('patronymicP').innerHTML=patronymic;
-  document.getElementById('specializationP').innerHTML=specialization;
-  document.getElementById('experienceP').innerHTML=experience;
+  document.getElementById('dataInput').innerText='Данные из input:';
+  document.getElementById('surnameP').innerText=surname;
+  document.getElementById('nameP').innerText=name;
+  document.getElementById('patronymicP').innerText=patronymic;
+  document.getElementById('specializationP').innerText=specialization;
+  document.getElementById('experienceP').innerText=experience;  
+
+  const jsonData = JSON.stringify({Фамилия:surname, Имя: name, Отчество: patronymic, Специальность: specialization, Опыт: experience}); 
+  document.getElementById('jsonName').innerText='Данные из json:';
+  document.getElementById('jsonDate').innerText=jsonData;
   popup.style.display = '';
-  
 }  
 
+window.addEventListener("click", function(event) { 
+  const popup = document.getElementById('popup');
+  if (event.target === popup) {
+      popup.style.display = "none";    
+  }
+});
+
+function setJSON (){
+  const jsonData = JSON.stringify({Фамилия:'surname'}); 
+  document.getElementById('jsonDate').innerText='Данные из json: ' + jsonData;
+}
